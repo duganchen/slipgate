95bf32d280619ba3c697a3dd45ede68f1f3a9e60
Dugan Chen <thedoogster@gmail.com>
2019-08-15 07:13:24 -0700

    Comment for the shape of the data

--- ts/main.ts
+++ ts/main.ts
@@ -68,6 +68,26 @@ app.on('ready', async () => {
 
     const buffer = await fs.readFile(dbFile)
     const xml = await buffer.toString()
+    /*
+    One map looks like this:
+    {
+        author: 'sock',
+        title: 'One Thousand Cuts',
+        md5sum: '8e03d6c9202136c89d99707ab97a1020',
+        size: 10396,
+        date: '15.08.14',
+        description: "Expanded standalone version of sock's map from <a " +
+          'href="func_mapjam2.html">Func Map Jam 2</a>: a medium-sized, ' +
+          'IKblue-themed, partially ruined temple with two individual ' +
+          'routes for replayablility. It comes with a set of ' +
+          'medieval-style item boxes.',
+        techinfo: {
+          zipbasedir: '1000cuts/',
+          commandline: '-game 1000cuts',
+          startmap: 'start'
+        }
+      }
+      */
     console.log(parser.parse(xml)['files']['file'][0])
 });
 
