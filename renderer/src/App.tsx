df3f0566177f8412a40938c27ae6f49f3a0a0960
Dugan Chen <thedoogster@gmail.com>
2020-08-31 06:23:25 -0700

    Drawer works but is laggy with all the maps

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 import MapList from "./components/MapList";
-import { AppBar, IconButton, Toolbar, Grid } from "@material-ui/core";
+import { AppBar, Drawer, IconButton, Toolbar, Grid } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import "./App.css";
 
 function App() {
   const [mapDetails, setMapDetails] = useState("");
+  const [drawerIsOpen, setDrawerIsOpen] = useState(true);
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton>
-            <MenuIcon />
+            <MenuIcon
+              onClick={() => {
+                console.log("clicked");
+                setDrawerIsOpen(!drawerIsOpen);
+              }}
+            />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="right"
+        open={drawerIsOpen}
+        onClose={() => {
+          console.log("closing");
+          setDrawerIsOpen(false);
+        }}
+      >
+        <p>This is in the drawer</p>
+      </Drawer>
       <Grid container direction="row" alignItems="stretch" className="AppGrid">
         <Grid item xs={6}>
           <MapList detailsSetter={setMapDetails} />
