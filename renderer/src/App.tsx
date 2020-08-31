f6a18fd323396f6d90ba6f662a3539caa659e38d
Dugan Chen <thedoogster@gmail.com>
2020-08-30 17:33:24 -0700

    We have an app bar with a menu

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
 import MapList from "./components/MapList";
+import { AppBar, IconButton, Toolbar, Grid } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import "./App.css";
 
 function App() {
   const [mapDetails, setMapDetails] = useState("");
 
   return (
-    <Grid container direction="row" alignItems="stretch" className="AppGrid">
-      <Grid item xs={6}>
-        <MapList detailsSetter={setMapDetails} />
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Grid container direction="row" alignItems="stretch" className="AppGrid">
+        <Grid item xs={6}>
+          <MapList detailsSetter={setMapDetails} />
+        </Grid>
+        <Grid item xs={6}>
+          <p dangerouslySetInnerHTML={{ __html: mapDetails }}></p>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <p dangerouslySetInnerHTML={{ __html: mapDetails }}></p>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
