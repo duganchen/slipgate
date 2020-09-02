72e9701058ab1fddcb92b5d611d8748474ba7e56
Dugan Chen <thedoogster@gmail.com>
2020-09-02 07:32:11 -0700

    App bar is done

--- renderer/src/components/Slipgate/Slipgate.tsx
+++ renderer/src/components/Slipgate/Slipgate.tsx
@@ -1,28 +1,37 @@
-import React, { useState } from "react";
-import { AppBar, Box, Tabs, Tab, Toolbar } from "@material-ui/core";
+import React from "react";
+import {
+  AppBar,
+  Checkbox,
+  Box,
+  FormControlLabel,
+  IconButton,
+  Input,
+  Switch,
+  Toolbar,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 import Packages from "../Packages";
-import Configuration from "../Configuration";
 
 export const Slipgate = () => {
-  const [tabIndex, setTabIndex] = useState(0);
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Tabs
-            value={tabIndex}
-            onChange={(value, newValue) => {
-              setTabIndex(newValue);
-            }}
-          >
-            <Tab label="Packages" />
-            <Tab label="Engine Configuration" />
-          </Tabs>
+          <IconButton>
+            <Menu />
+            <Input type="search" placeholder="filter" />
+            <FormControlLabel control={<Switch />} label="Installed Only" />
+            <FormControlLabel control={<Checkbox />} label="Sort by rating" />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Sort by release date"
+            />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Box flexDirection="column" display="flex" height="100%">
         <Toolbar />
-        {tabIndex ? <Configuration /> : <Packages />}
+        <Packages />
       </Box>
     </>
   );
