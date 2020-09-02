1e13020c0e16ec50713ba87002b524e4e9849105
Dugan Chen <thedoogster@gmail.com>
2020-09-02 07:48:57 -0700

    Menu is there but opens in the wrong place

--- renderer/src/components/Slipgate/Slipgate.tsx
+++ renderer/src/components/Slipgate/Slipgate.tsx
@@ -6,19 +6,33 @@ import {
   FormControlLabel,
   IconButton,
   Input,
+  Menu,
+  MenuItem,
   Switch,
   Toolbar,
 } from "@material-ui/core";
-import { Menu } from "@material-ui/icons";
+import MenuIcon from "@material-ui/icons/Menu";
 import Packages from "../Packages";
 
 export const Slipgate = () => {
+  const [anchorEl, setAnchorEl] = React.useState<EventTarget | null>(null);
+
+  const open = Boolean(anchorEl);
   return (
     <>
       <AppBar>
         <Toolbar>
-          <IconButton>
-            <Menu />
+          <IconButton
+            onClick={(event) => {
+              setAnchorEl(event.target);
+            }}
+          >
+            <MenuIcon />
+            <Menu open={open}>
+              <MenuItem>Configure</MenuItem>
+              <MenuItem>Reload Database</MenuItem>
+              <MenuItem>Exit</MenuItem>
+            </Menu>
             <Input type="search" placeholder="filter" />
             <FormControlLabel control={<Switch />} label="Installed Only" />
             <FormControlLabel control={<Checkbox />} label="Sort by rating" />
