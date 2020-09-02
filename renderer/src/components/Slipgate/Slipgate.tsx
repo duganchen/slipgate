e47c289815370e9534f91825ebdae7e2728dc58e
Dugan Chen <thedoogster@gmail.com>
2020-09-02 08:48:24 -0700

    We have the configuration dialog

--- renderer/src/components/Slipgate/Slipgate.tsx
+++ renderer/src/components/Slipgate/Slipgate.tsx
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import {
   AppBar,
   Checkbox,
+  Button,
   Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   FormControlLabel,
   IconButton,
   Input,
   Menu,
   MenuItem,
   Switch,
+  TextField,
   Toolbar,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -16,9 +22,44 @@ import Packages from "../Packages";
 
 export const Slipgate = () => {
   const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
 
   return (
     <>
+      <Dialog
+        open={dialogIsOpen}
+        onClose={() => {
+          setDialogIsOpen(false);
+        }}
+      >
+        <DialogTitle>Configure Slipgate</DialogTitle>
+        <DialogContent>
+          <div>
+            <TextField label="Quake Executable" />
+            <Button>Browse</Button>
+          </div>
+          <div>
+            <TextField label="Quake Directory" />
+            <Button>Browse</Button>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setDialogIsOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              setDialogIsOpen(false);
+            }}
+          >
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
       <AppBar>
         <Toolbar>
           <IconButton
@@ -38,6 +79,7 @@ export const Slipgate = () => {
           >
             <MenuItem
               onClick={() => {
+                setDialogIsOpen(true);
                 setAnchorEl(null);
               }}
             >
