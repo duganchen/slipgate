51f40e373f00ecbc29457bbdf344606fca6426e5
Dugan Chen <thedoogster@gmail.com>
2020-09-06 12:00:50 -0700

    Remove an unneeded Box

--- renderer/src/components/Packages/Packages.tsx
+++ renderer/src/components/Packages/Packages.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-// import PackageInteraction from "../PackageInteraction";
+import PackageInteraction from "../PackageInteraction";
 import PackageList from "../PackageList";
 
 export const Packages = () => {
   return (
     <Box display="flex" overflow="hidden" flexGrow={1}>
       <Box flexGrow={1} display="flex" flexDirection="row">
-        <Box flexGrow={1} overflow="auto">
-          <PackageList />
-        </Box>
+        <PackageList />
         <Box flexGrow={2}>
-          <p>Column 2</p>
+          <PackageInteraction />
         </Box>
       </Box>
     </Box>
