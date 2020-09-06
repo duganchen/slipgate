dda4e5c16ee30aa881947cae2898e2164352373a
Dugan Chen <thedoogster@gmail.com>
2020-09-06 09:23:44 -0700

    Test with 10K items

--- renderer/src/components/PackageList/PackageList.tsx
+++ renderer/src/components/PackageList/PackageList.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Container, List, ListItem } from "@material-ui/core";
 
 export const PackageList = () => {
+  let numbers = [];
+  for (let i = 0; i < 10000; i++) {
+    numbers.push(<ListItem>`item {i}`</ListItem>);
+  }
   return (
     <Container>
-      <List>
-        <ListItem>
-          <div>Item 1</div>
-        </ListItem>
-        <ListItem>
-          <div>Item 2</div>
-        </ListItem>
-      </List>
+      <List>{numbers}</List>
     </Container>
   );
 };
