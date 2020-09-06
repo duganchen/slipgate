ea93c752a5b143634c2a8b8711bf7b526c11f58b
Dugan Chen <thedoogster@gmail.com>
2020-09-06 16:01:15 -0700

    Use react-window

--- renderer/src/components/PackageList/PackageList.tsx
+++ renderer/src/components/PackageList/PackageList.tsx
@@ -1,27 +1,29 @@
 import React from "react";
-import { Container, List, ListItem, ListItemText } from "@material-ui/core";
+import { ListItem, ListItemText } from "@material-ui/core";
 import { FixedSizeList } from "react-window";
 import { AutoSizer } from "react-virtualized";
-import { makeStyles } from "@material-ui/core/styles";
 
 export const PackageList = () => {
-  const numbers = [];
-  for (let i = 0; i < 99; i++) {
-    numbers.push(i);
-  }
+  // This is the "height" in the inspector if you don't use react-window.
+  // Good?
+  const itemHeight = 72;
 
-  const items = numbers.map((item) => (
-    <ListItem key={item}>
-      <ListItemText primary={`item ${item}`} />
-    </ListItem>
-  ));
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: "100%",
-      height: "100%",
-    },
-  }));
-  const classes = useStyles();
-  return <List className={classes.root}>{items}</List>;
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <FixedSizeList
+          height={height}
+          itemCount={10000}
+          itemSize={itemHeight}
+          width={width}
+        >
+          {({ index, style }) => (
+            <ListItem key={index} style={style}>
+              <ListItemText primary={`Row ${index}`} />
+            </ListItem>
+          )}
+        </FixedSizeList>
+      )}
+    </AutoSizer>
+  );
 };
