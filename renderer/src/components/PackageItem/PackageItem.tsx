4baa4a1e3d757b4590a1ad87e73f057fdd765a5b
Dugan Chen <thedoogster@gmail.com>
2020-09-07 09:10:37 -0700

    The map list is fully restored

--- renderer/src/components/PackageItem/PackageItem.tsx
+++ renderer/src/components/PackageItem/PackageItem.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { ListItem, ListItemText } from "@material-ui/core";
+import { Grid, ListItem, ListItemText } from "@material-ui/core";
 import { QuakeMap } from "../types";
+import { renderAuthors } from "../MapList/helpers";
+import Rating from "@material-ui/lab/Rating";
 
 export const PackageItem = (props: {
   style: React.CSSProperties;
   map: QuakeMap;
 }) => {
   return (
-    <ListItem style={props.style}>
-      <ListItemText primary={props.map.description} />
+    <ListItem key={props.map.id} alignItems="flex-start" button>
+      <Grid container>
+        <Grid item xs>
+          <ListItemText
+            primary={props.map.title}
+            secondary={renderAuthors(props.map.authors)}
+          />
+        </Grid>
+        <Grid item className="MapList__Item-Secondary">
+          <Rating value={props.map.rating} readOnly size="small" />
+        </Grid>
+      </Grid>
     </ListItem>
   );
 };
